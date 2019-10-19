7cec76759e43e0d1f480b0a500a4e33c0b35ab39
NickDGreg <nick.d.gregory@hotmail.com>
2019-10-19 20:27:14 +0200

    move function to js file

--- public/js/counters/basicCounter.js
+++ public/js/counters/basicCounter.js
@@ -1,5 +1,8 @@
-window.onload = myFunction;
-    function myFunction() {
-      document.getElementById("co2").innerHTML = Date.now();
-      setTimeout(myFunction, 1);
-    }
+//  window.onload = myFunction;
+function myFunction() {
+    document.getElementById(thisScript.getAttribute("element")).innerHTML = Date.now();
+    setTimeout(myFunction, 1);
+}
+
+var thisScript = document.currentScript;
+myFunction();
